868e6eb11db8e7e2dfb1ab483312b470b9efdb9b
Stanford J Gadaga <118995806+StanGadaga@users.noreply.github.com>
2022-12-13 19:46:25 +0530

    Add files via upload

---
+++ projecthtml/script.js
@@ -0,0 +1,8 @@
+function abc(){
+    window.alert("YOUR DATA IS BEING PROCCESSED")
+    document.write("BE PATIENT YOU WILL RECEIVE YOUR BLOOD SOON")
+}
+function cdf(){
+    window.alert("THANK YOU FOR YOUR SERVICE")
+    document.write("WE REALLY APPRECIATE YOUR HELP TO SAVE LIFE BY DONATING YOUR BLOOD")
+}
